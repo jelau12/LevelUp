100e32f99693166947f11984b81c2f63fe481892
Jesper Lauridsen <Jelauwow@gmail.com>
2022-04-25 11:34:38 +0200

    Made Index action in controller

--- LevelUp.Web/Controllers/ProductController.cs
+++ LevelUp.Web/Controllers/ProductController.cs
@@ -1,16 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using LevelUp.Web.Models;
 
 namespace LevelUp.Web.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        string Baseurl = "https://localhost:44350";
+
+        // GET: Products
+        #region Consume Get Method
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Product> products = new List<Product>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetProducts using HttpClient
+                HttpResponseMessage responeMsg = await client.GetAsync("api/Products/GetProducts");
+
+                //Checking the response is successful or not which is sent using HttpClient
+                if (responeMsg.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var productResponse = responeMsg.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    products = JsonConvert.DeserializeObject<List<Product>>(productResponse);
+                }
+            }
+
+            return View(products);
         }
+        #endregion
     }
 }
