e550ad00a344395da4f68647b67be5c9f2ec7430
Jesper Lauridsen <Jelauwow@gmail.com>
2022-04-25 11:02:05 +0200

    Made instance of LevelUpDbContext in controller

---
+++ LevelUp.Api/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using LevelUp.Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LevelUp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //fields
+        private LevelUpDbContext _context;
+
+        //constructer
+        public ProductsController(LevelUpDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
