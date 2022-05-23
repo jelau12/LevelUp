a90831b975d8930650be10cbc2be58d037a016c1
Jesper Lauridsen <Jelauwow@gmail.com>
2022-05-23 11:53:42 +0200

    Change default start up route

--- LevelUp.Web/Startup.cs
+++ LevelUp.Web/Startup.cs
@@ -46,7 +46,7 @@ namespace LevelUp.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
             });
         }
     }
