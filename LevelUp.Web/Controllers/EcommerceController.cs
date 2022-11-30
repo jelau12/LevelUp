748e58bf5d30dd4ab12c165848b4543cd178c24f
jelau12 <jelauwow@gmail.com>
2022-11-30 19:23:10 +0100

    Added EcommerceController and view

---
+++ LevelUp.Web/Controllers/EcommerceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LevelUp.Web.Controllers
+{
+    public class EcommerceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
